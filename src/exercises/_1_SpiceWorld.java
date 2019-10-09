170cf850368c7b04bb437727f6f5179deaa2fc63
Barezi79 <branislavnesic79@gmail.com>
2019-10-09 11:49:02 +0200

    SpiceWorld second change

--- src/exercises/_1_SpiceWorld.java
+++ src/exercises/_1_SpiceWorld.java
@@ -1,11 +1,16 @@
 package exercises;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 public class _1_SpiceWorld {
 	/*
 	 * Replace the anonymous inner class with a lambda expression.
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * SpiceGirl sporty = new SpiceGirl() {
 		 * 
@@ -15,6 +20,11 @@ public class _1_SpiceWorld {
 		 */
 		SpiceGirl sporty = () -> System.out.println("Do backflips.");
 		sporty.perform();
+		
+		File file = new File("src/copyright");
+		file.createNewFile();
+		System.out.println(file.getAbsolutePath());
+		FileUtils.write(file, "Copyright Branislav Nesic, 2019");
 	}
 }
 
